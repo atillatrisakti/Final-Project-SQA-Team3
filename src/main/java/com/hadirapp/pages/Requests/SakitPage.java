cb5a44628e5a309787d06806ac9582f155f47bbf
atillatrisakti <atillasilvaaa@gmail.com>
2026-05-05 22:04:34 +0700

    merging Sakit module

--- src/main/java/com/hadirapp/pages/Requests/SakitPage.java
+++ src/main/java/com/hadirapp/pages/Requests/SakitPage.java
@@ -1,5 +1,168 @@
 package com.hadirapp.pages.Requests;
 
-public class SakitPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.hadirapp.utlis.WaitUtils;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SakitPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    // Locators
+    @FindBy(xpath = "//a[@class='user__menu__item']//p[text()='Sakit']")
+    public WebElement menuSakit;
+
+    @FindBy(xpath = "//button[contains(text(), 'Ajukan Sakit')]")
+    public WebElement btnAjukanSakit;
+
+    @FindBy(xpath = "//*[local-name()='svg' and @data-testid='AccessAlarmIcon']")
+    public WebElement iconJamKalender;
+
+    @FindBy(xpath = "//input[@type='file']")
+    public WebElement inputUploadFile;
+
+    @FindBy(xpath = "//button[text()='Simpan']")
+    public WebElement btnSimpanKalender;
+
+    @FindBy(xpath = "//button[@type='submit' and text()='Ajukan']")
+    public WebElement btnSubmit;
+
+    @FindBy(xpath = "//button[text()='Reset']")
+    public WebElement btnReset;
+
+    @FindBy(xpath = "//span[contains(text(), '.png')]")
+    public WebElement labelFileName;
+
+    @FindBy(xpath = "//input[@placeholder='Pilih Tanggal']")
+    public WebElement inputTanggal;
+
+    @FindBy(xpath = "//div[contains(@class, 'MuiBox-root')]//p[text()=' / ' or text()='/']")
+    public WebElement textTanggal;
+
+    @FindBy(xpath = "//div[@role='alert']//div[contains(text(), 'sudah melakukan izin sakit')]")
+    public WebElement toastErrorDuplicate;
+
+    @FindBy(xpath = "//p[contains(@class, 'MuiFormHelperText-root')]")
+    public List<WebElement> listErrorMessages;
+
+    @FindBy(xpath = "//div[@role='alert']//div[contains(text(), 'Incorrect datetime')]")
+    public WebElement toastErrorInvalidDate;
+
+    @FindBy(xpath = "//div[contains(@class, 'MuiSnackbarContent-message')]")
+    public WebElement snackbarErrorMsg;
+
+
+    // Constructor
+    public SakitPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
+    }
+
+    // Method untuk memilih rentang tanggal sakit
+    public void pilihRentangTanggal(String tglMulai, String tglSelesai) {
+        WaitUtils.waitForElementClickable(driver, iconJamKalender, 0).click();
+        clickTanggal(tglMulai);
+        clickTanggal(tglSelesai);
+        WaitUtils.waitForElementClickable(driver, btnSimpanKalender, 0).click();
+    }
+
+    // method pilih tanggal
+    private void clickTanggal(String tgl) {
+        String xpathTgl = String.format("//button[contains(@class, 'rdrDay')]//span[text()='%s']", tgl);
+
+        try {
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpathTgl)));
+            element.click();
+        } catch (Exception e) {
+            WebElement element = driver.findElement(By.xpath(xpathTgl));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
+    }
+
+    public void klikMenuSakit(){
+        WaitUtils.waitForElementClickable(driver, menuSakit, 10);
+        try {
+            menuSakit.click();
+        } catch (Exception e) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", menuSakit);
+        }
+    }
+
+
+    // Method untuk upload dokumen
+    public void uploadDokumen(String path) {
+        inputUploadFile.sendKeys(path);
+    }
+
+    // Method untuk klik tombol Ajukan
+    public void klikTombolAjukan() {
+        try {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", btnSubmit);
+            Thread.sleep(500);
+
+            WaitUtils.waitForElementClickable(driver, btnSubmit, 10).click();
+        } catch (Exception e) {
+            System.out.println("Klik normal gagal, mencoba JavaScript Click...");
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", btnSubmit);
+        }
+    }
+
+    // Method untuk mendapatkan teks jika tanggal kosong
+    public String getTeksTanggal() {
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOf(textTanggal));
+            return element.getText().trim();
+        } catch (Exception e) {
+            WebElement element = driver
+                    .findElement(By.xpath("//label[text()='Pilih Tanggal']/following-sibling::div//p"));
+            return element.getText().trim();
+        }
+    }
+
+    // Method untuk klik tombol Reset
+    public void klikReset() {
+        WaitUtils.waitForElementClickable(driver, btnReset, 10).click();
+    }
+
+    public String getErrorMessageText() {
+        try {
+            WaitUtils.waitForElementVisible(driver, snackbarErrorMsg, 5);
+            return snackbarErrorMsg.getText();
+        } catch (Exception e) {
+            return "Error message tidak muncul";
+        }
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        try {
+            WaitUtils.waitForElementVisible(driver, snackbarErrorMsg, 10);
+            return snackbarErrorMsg.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Method untuk mendapatkan semua pesan error yang muncul
+    public List<String> getAllErrorMessages() {
+        wait.until(ExpectedConditions.visibilityOfAllElements(listErrorMessages));
+        List<String> messages = new ArrayList<>();
+        for (WebElement error : listErrorMessages) {
+            messages.add(error.getText());
+        }
+        return messages;
+    }
 }
--- src/test/java/com/hadirapp/tests/requests/SakitTest.java
+++ src/test/java/com/hadirapp/tests/requests/SakitTest.java
@@ -1,5 +1,117 @@
 package com.hadirapp.tests.requests;
 
-public class SakitTest {
-    
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.hadirapp.base.BaseTest;
+import com.hadirapp.pages.Auth.LoginPage;
+import com.hadirapp.pages.Requests.SakitPage;
+import com.hadirapp.utlis.Constants;
+import com.hadirapp.utlis.WaitUtils;
+
+public class SakitTest extends BaseTest {
+    private LoginPage loginPage;
+    private SakitPage sakitPage;
+
+    @BeforeMethod
+    public void setUp(){
+        loginPage = new LoginPage(driver);
+        sakitPage = new SakitPage(driver);
+        loginPage.doLogin(Constants.EMAIL_3, Constants.PASSWORD_3);
+        WaitUtils.waitForUrlContains(driver, "apps", 20);
+        sakitPage.klikMenuSakit();
+        WaitUtils.waitForUrlContains(driver, "sick", 20);
+    }
+
+    @Test(priority = 1, description = "TC-SKT-01 - Verifikasi navigasi ke halaman Sakit")
+    public void testNavigasiSakit() {
+        log.info("TC-SKT-01 - Navigasi ke halaman Sakit");
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("sick"),
+                "URL saat ini bukan Halaman Sakit: " + currentUrl);
+    }
+
+    @Test(priority = 2, description = "TC-SKT-02 - Sukses ajukan sakit dengan data valid")
+    public void testAjukanSakitValid() {
+        log.info("TC-SKT-02 - Ajukan sakit dengan data valid");
+        sakitPage.btnAjukanSakit.click();
+        sakitPage.pilihRentangTanggal("6", "8");
+        String projectPath = System.getProperty("user.dir");
+        String imagePath = projectPath + "\\src\\test\\resources\\testdata\\surat_sakit.png";
+        sakitPage.uploadDokumen(imagePath);
+        sakitPage.btnSubmit.click();
+        WaitUtils.waitForUrlContains(driver, "sick", 20);
+        Assert.assertTrue(driver.getCurrentUrl().contains("sick"));
+    }
+
+    @Test(priority = 3, description = "TC-SKT-03 - Ajukan sakit dengan Foto Kosong")
+    public void testAjukanSakitUploadKosong() {
+        log.info("TC-SKT-03 - Ajukan sakit dengan Foto Kosong");
+        sakitPage.btnAjukanSakit.click();
+        sakitPage.pilihRentangTanggal("6", "8");
+        sakitPage.btnSubmit.click();
+        Assert.assertTrue(sakitPage.isErrorMessageDisplayed(), "Error message tidak tampil!");
+        String actualError = sakitPage.getErrorMessageText();
+        String expectedError = "Photo is Required";
+        Assert.assertEquals(actualError, expectedError, "Pesan error tidak sesuai atau tidak muncul!");
+    }
+
+    @Test(priority = 4, description = "TC-SKT-04 - Pengajuan sakit dengan tanggal kosong")
+    public void testSakitTanggalKosong() {
+        log.info("TC-SKT-04 - Pengajuan sakit dengan tanggal kosong");
+        sakitPage.btnAjukanSakit.click();
+        String projectPath = System.getProperty("user.dir");
+        String imagePath = projectPath + "\\src\\test\\resources\\testdata\\surat_sakit.png";
+        sakitPage.uploadDokumen(imagePath);
+        sakitPage.btnSubmit.click();
+        Assert.assertTrue(sakitPage.isErrorMessageDisplayed(), "Error message tidak tampil!");
+        String actualError = sakitPage.getErrorMessageText();
+        String expectedError = "Incorrect datetime value: 'Invalid date' for column 'sick_request_date_from' at row 1";
+        Assert.assertEquals(actualError, expectedError, "Pesan error tidak sesuai atau tidak muncul!");
+    }
+
+    @Test(priority = 5, description = "TC-SKT-05 - Pengajuan sakit dengan semua field kosong")
+    public void testSakitSemuaFieldKosong() {
+        log.info("TC-SKT-05 - Pengajuan sakit dengan semua field kosong");
+        sakitPage.btnAjukanSakit.click();
+        sakitPage.btnSubmit.click();
+        Assert.assertTrue(sakitPage.isErrorMessageDisplayed(), "Error message tidak tampil!");
+        String actualError = sakitPage.getErrorMessageText();
+        String expectedError = "Incorrect datetime value: 'Invalid date' for column 'sick_request_date_from' at row 1";
+        Assert.assertEquals(actualError, expectedError, "Pesan error tidak sesuai atau tidak muncul!");
+    }
+
+    @Test(priority = 6, description = "TC-SKT-06 - Error saat input tanggal yang sudah pernah diajukan")
+    public void testSakitDataDuplikat() {
+        log.info("TC-SKT-06 - Error saat input tanggal yang sudah pernah diajukan");
+        sakitPage.btnAjukanSakit.click();
+        sakitPage.pilihRentangTanggal("6", "8");
+        String projectPath = System.getProperty("user.dir");
+        String imagePath = projectPath + "\\src\\test\\resources\\testdata\\surat_sakit.png";
+        sakitPage.uploadDokumen(imagePath);
+        sakitPage.klikTombolAjukan();
+        Assert.assertTrue(sakitPage.isErrorMessageDisplayed(), "Error message tidak tampil!");
+        String actualError = sakitPage.getErrorMessageText();
+        String expectedError = "Anda sudah melakukan izin sakit di tanggal tersebut";
+        Assert.assertEquals(actualError, expectedError, "Pesan error tidak sesuai atau tidak muncul!");
+    }
+
+    @Test(priority = 7, description = "TC-SKT-07 - Memastikan tombol Reset mengosongkan form")
+    public void testResetForm() {
+        log.info("TC-SKT-07 - Memastikan tombol Reset mengosongkan form");
+        sakitPage.btnAjukanSakit.click();
+        sakitPage.pilihRentangTanggal("10", "12");
+        String projectPath = System.getProperty("user.dir");
+        String imagePath = projectPath + "\\src\\test\\resources\\testdata\\surat_sakit.png";
+        sakitPage.uploadDokumen(imagePath);
+        sakitPage.klikReset();
+        String valueTanggal = sakitPage.getTeksTanggal();
+        System.out.println("Teks tanggal yang didapat sekarang: '" + valueTanggal + "'");
+        Assert.assertTrue(valueTanggal.contains("/"),
+                "Harusnya dapet '/', tapi malah dapet: " + valueTanggal);
+    }
 }
